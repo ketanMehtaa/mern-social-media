89fabc9fa41e378fa803cd8118c45619625b827a
ketan mehta <meetketanmehta@gmail.com>
2024-07-04 02:57:46 +0530

    http changed to https

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://social.ketan.cloud/auth/login", {
+    const loggedInResponse = await fetch("https://social.ketan.cloud/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
